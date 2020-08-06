12df8717fd8dfbffccbfba1e094abc4b55b8dcf2
chris-carstens <ctcarstens@uc.cl>
2020-08-06 04:29:24 -0400

    adding views and change color

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,19 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/awards')
+def awards():
+    return render_template('awards.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
