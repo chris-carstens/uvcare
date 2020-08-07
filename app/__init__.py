489cfd4c8e7aec33732a593ee9249fbc48aeaffb
chris-carstens <ctcarstens@uc.cl>
2020-08-06 21:34:37 -0400

    footer and team update

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,12 @@ from flask_assets import Environment, Bundle
 app = Flask(__name__) 
 assets = Environment(app)
 assets.url = app.static_url_path
+
 scss = Bundle('index.scss', filters='pyscss', output='all.css')
 assets.register('scss_all', scss)
+
+scss_team = Bundle('teams.scss', filters='pyscss', output='team.css')
+assets.register('scss_teams', scss_team)
 
 from app import routes
 
